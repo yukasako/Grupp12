29794cbbbb789614fbdb56668130723d33f6f74d
Jonas Rahlin <ralin.jonas@gmail.com>
2023-10-22 11:10:01 +0200

    Added a scroll lock for active nav.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const ham = document.getElementById("hamburger");
 const nav = document.getElementById("navbar-links");
+const body = 
 
 ham.addEventListener("click", ()=>{
     nav.classList.toggle("display");
+    document.body.classList.toggle("scrollLock");
 })
